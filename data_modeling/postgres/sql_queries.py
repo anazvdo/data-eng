637d05efa17da07222e4f7ccb58400d10d0ad838
ana <ana.caroline.reis@usp.br>
2020-04-26 21:22:11 -0300

    Starting Inserts

--- data_modeling/postgres/sql_queries.py
+++ data_modeling/postgres/sql_queries.py
@@ -1,12 +1,54 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE songplays"
-user_table_drop = "DROP TABLE users"
-song_table_drop = "DROP TABLE songs"
-artist_table_drop = "DROP TABLE artists"
-time_table_drop = "DROP TABLE time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
+
+user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users(
+        user_id int PRIMARY KEY NOT NULL,
+        first_name varchar,
+        last_name varchar,
+        gender varchar,
+        level varchar
+    )
+""")
+
+artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists(
+        artist_id varchar PRIMARY KEY,
+        name varchar,
+        location varchar,
+        latitude double precision,
+        longitude double precision
+    )
+""")
+
+time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time(
+        start_time timestamp PRIMARY KEY,
+        hour int,
+        day int,
+        week int,
+        month int,
+        year int,
+        weekday varchar
+    )
+""")
+
+song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs(
+        song_id varchar PRIMARY KEY NOT NULL,
+        title varchar,
+        artist_id varchar REFERENCES artists(artist_id),
+        year int,
+        duration double precision
+    )
+""")
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays(
@@ -22,47 +64,6 @@ songplay_table_create = ("""
     )
 """)
 
-user_table_create = ("""
-    CREATE TABLE IF NOT EXISTS users(
-        user_id int PRIMARY KEY NOT NULL,
-        first_name varchar,
-        last_name varchar,
-        gender varchar,
-        level varchar
-    )
-""")
-
-song_table_create = ("""
-    CREATE TABLE IF NOT EXISTS songs(
-        song_id varchar PRIMARY KEY NOT NULL,
-        title varchar,
-        artist_id varchar REFERENCES artists(artist_id),
-        year int,
-        duration double
-    )
-""")
-
-artist_table_create = ("""
-    CREATE TABLE IF NOT EXISTS artists(
-        artist_id varchar PRIMARY KEY,
-        name varchar,
-        location varchar,
-        latitude double,
-        longitude double
-    )
-""")
-
-time_table_create = ("""
-    CREATE TABLE IF NOT EXISTS time(
-        start_time datetime PRIMARY KEY,
-        hour int,
-        day int,
-        week int,
-        month int,
-        year int,
-        weekday varchar
-    )
-""")
 
 # INSERT RECORDS
 
@@ -73,6 +74,8 @@ user_table_insert = ("""
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
@@ -89,5 +92,5 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, artist_table_create, time_table_create, song_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
